1b552cab58f6706fa602ef7f46b54f59e416967d
Robert Scully <robert.p.scully@gmail.com>
2021-06-30 14:21:55 +0100

    Update main.tf
    changing to my name

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ terraform {
 
 
 provider "google" {
-  project     = "tharris-vault-customer-pov"
+  project     = "robertscully-vault-customer-pov"
   region      = "europe-west2"
   zone        = "europe-west2-a"
 }
